a62c8d1f138b242f14da7ff473dac751791c49b7
archana <archanarajasekaran02@gmail.com>
2026-07-06 12:58:43 +0530

    hi

---
+++ src/org/edu/Arts.java
@@ -0,0 +1,30 @@
+package org.edu;
+//child class
+	public class Arts extends Education{
+		 @Override
+		public void undergraduate() {
+			// TODO Auto-generated method stub
+			super.undergraduate();}
+		 @Override
+		 public void postgraduate() {
+			// TODO Auto-generated method stub
+			super.postgraduate();
+		 }
+		 public void bsc() {
+			 System.out.println("BSc Course");}
+		public void bED() {
+		System.out.println("BEd Course");
+		}
+		public void ba() {
+			System.out.println("BA Course");
+			
+		} public static void main(String[]args) {
+			 Arts a = new Arts();
+			 a.postgraduate();
+			 a.undergraduate();
+			 a.ba();
+			 a.bED();
+			 a.bsc();
+		 }
+		 }
+	
---
+++ src/org/edu/Education.java
@@ -0,0 +1,16 @@
+package org.edu;
+
+public  abstract class Education {
+	//abstraction
+	//non abstract method
+	public void undergraduate() {
+		System.out.println("UG Course-Education");
+	
+	}
+
+	
+	public void  postgraduate() {
+		System.out.println("PG Course-Education");
+	}
+
+}
